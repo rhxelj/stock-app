7bafe1a88ab234ed1cecece7f1feb844fb51dd07
Rogelio Martin Corsino <rcorsino@gmail.com>
2019-03-21 09:21:21 -0300

    Ruteo de StkGrupo
    Agregar el menu de grupo y algunas modificaciones en el componente de StkGrupo.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,6 +49,7 @@ class  Header extends React.Component {
                     <MenuItem onClick={this.handleClose}><Link to="/Proveedores">Proveedores</Link></MenuItem>
                     <MenuItem onClick={this.handleClose}><Link to="/StkUnMed">Unidad Medida</Link></MenuItem>
                     <MenuItem onClick={this.handleClose}><Link to="/StkRubro">Rubros</Link></MenuItem>
+                    <MenuItem onClick={this.handleClose}><Link to="/StkGrupo">Grupos</Link></MenuItem>
                     </Menu>
                 </AppBar>
             </div>   
