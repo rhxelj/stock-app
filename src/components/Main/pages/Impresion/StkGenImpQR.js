46bd281a2eb371cb1bd06ca2cd20bd27146d0e6d
Rogelio Martin Corsino <rcorsino@gmail.com>
2019-09-02 23:00:56 -0300

    implementar layout de impresion de forma tabular

--- src/components/Main/pages/Impresion/StkGenImpQR.js
+++ src/components/Main/pages/Impresion/StkGenImpQR.js
@@ -8,6 +8,39 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { withStyles } from '@material-ui/core/styles'; 
+import Typography from '@material-ui/core/Typography'; 
+
+const CustomTableCell = withStyles(theme => ({
+  head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+  },
+  body: {
+      fontSize: 14,
+  },
+}))(TableCell);
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+  row: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+});
 
 
 class StkGenImpQR extends Component {
@@ -23,7 +56,7 @@ class StkGenImpQR extends Component {
           open: true,
       }
     }     
-      stkenvaseleeimp = _ => {
+    stkenvaseleeimp = _ => {
         const url = IpServidor +  '/stkenvaseleeimp/?id='+this.props.ubicaG ; 
         console.log(url)
         request
@@ -34,7 +67,6 @@ class StkGenImpQR extends Component {
             // this.setState({envaseimp: envaseimp})
             this.setState(()=>{ return {envaseimp: envaseimp}});
             })
-           
     }
    stkenvasecambiaimp = _ => {
         const url = IpServidor +  '/stkenvasecambiaimp/?id='+this.props.ubicaG ; 
@@ -52,86 +84,74 @@ class StkGenImpQR extends Component {
     this.stkenvaseleeimp()  
   }      
 
-render () {
-
+  render () {
     return (
-  
-    <div >
-    <form>
-     
-     <br></br>
-     <br></br>
-     <br></br>
-      {/* <button type="button" onClick={()=>printJS({maxWidth : 800,  scanStyles : false, printable : 'mostrar', type : 'html'})}>
-        Imprime
-      </button>
+    <div>
+      <Paper >
+        <Table>
+            {/* <TableHead>
+                <TableRow className={this.props.classes.row} >
+                    <CustomTableCell className="headerFijo"  ></CustomTableCell>
+                    {columns.map((row, index) => {
+                        return (<CustomTableCell className="headerFijo"  key={index} onClick={() => { return row.order && this.sortBy(row.accessor) }} >{row.Header}</CustomTableCell>)
+                    })
+                    }
+                </TableRow>
+            </TableHead> */}
 
-      <button type="button" onClick={()=>this.props.cancelaImpresion()}>
-        Cancelar
-      </button> */}
+          <TableBody>
+            {this.state.envaseimp.map((option, indice) => {
+              return (
+                <TableRow>
+                  <CustomTableCell>BORRAR</CustomTableCell>
+                  <CustomTableCell>
+                    <Typography variant="body2" gutterBottom >Envase : { option.idStkEnvase }</Typography>
+                    <Typography variant="body2" gutterBottom >{ option.StkGrupoDesc  }</Typography>
+                    <Typography variant="body2" gutterBottom >{ option.StkRubroDesc }</Typography>
+                    <Typography variant="body2" gutterBottom >{ option.StkItemsDesc }</Typography>
+                    <Typography variant="body2" gutterBottom >Partida : { option.StkEnvasePartida }</Typography>
+                    <Typography variant="body2" gutterBottom >Cant. : { option.StkEnvaseCant  }</Typography>
+                    <Typography variant="body2" gutterBottom >Fecha : { option.StkEnvaseFechaAct  }</Typography>
+                    <Typography variant="body2" gutterBottom >Ubicación : { option.StkEnvaseUbG }  -- {option.StkEnvaseUbF} </Typography>
+                    <Typography variant="body2" gutterBottom >Observ. : { option.StkEnvaseObserv  }</Typography>
+                  </CustomTableCell>
+                  <CustomTableCell>
+                    <QRCode 
+                      id="codigo-QR"
+                      value={ option.idStkEnvase +  
+                        ':' + option.StkEnvaseGrupo + 
+                        ':' + option.StkEnvaseRubro + 
+                        ':' + option.StkEnvaseItem + 
+                        ': Envase Nro = ' + option.idStkEnvase +
+                        ' - ' + option.StkGrupoDesc + 
+                        ' - ' + option.StkRubroDesc + 
+                        ' - ' + option.StkItemsDesc + 
+                        ' - Cant ' + option.StkEnvaseCant + 
+                        ' - Ubic ' + option.StkEnvaseUbG + '--' + option.StkEnvaseUbF +
+                        ' - ' + option.stkenvasefecha + 
+                        ' - ' + option.StkEnvaseObserv
+                      } 
+                      style={{ width: 256 }}
+                      renderAs='svg'
+                    />
+                  </CustomTableCell>
+              </TableRow>
+              )
+            })
+          }
+          </TableBody>
+        </Table>
+      </Paper>
+    
+        <Button variant="contained" color="primary" onClick={()=>printJS({maxWidth : 800,  scanStyles : false, printable : 'mostrar', type : 'html'})}>
+          Imprime
+        </Button>
 
-<Button variant="contained" color="primary" onClick={()=>printJS({maxWidth : 800,  scanStyles : false, printable : 'mostrar', type : 'html'})}>
-        Imprime
-      </Button>
-
-      <Button variant="contained" color="secondary" onClick={()=>this.props.cancelaImpresion()}>
-        Cancelar
-      </Button>
-      
-      <Grid 
-        id = "mostrar"
-        container
-        direction="column"
-        // justify="left"
-        // alignItems="left"
-      >
-        {this.state.envaseimp.map((option, indice) => (
-        <Grid    xs={3} >
-          <QRCode 
-              id="codigo-QR"
-              value={ option.idStkEnvase +  
-                ':' + option.StkEnvaseGrupo + 
-                ':' + option.StkEnvaseRubro + 
-                ':' + option.StkEnvaseItem + 
-                ': Envase Nro = ' + option.idStkEnvase +
-                ' - ' + option.StkGrupoDesc + 
-                ' - ' + option.StkRubroDesc + 
-                ' - ' + option.StkItemsDesc + 
-                ' - Cant ' + option.StkEnvaseCant + 
-                ' - Ubic ' + option.StkEnvaseUbG + '--' + option.StkEnvaseUbF +
-                ' - ' + option.stkenvasefecha + 
-                ' - ' + option.StkEnvaseObserv
-                } 
-
-              style={{ width: 256 }}
-              renderAs='svg'
-              >
-        </QRCode> 
-          <Paper >Envase : { option.idStkEnvase }</Paper>
-          <Paper >{ option.StkGrupoDesc  }</Paper>
-          <Paper >{ option.StkRubroDesc }</Paper>
-          <Paper >{ option.StkItemsDesc }</Paper>
-          <Paper >Partida : { option.StkEnvasePartida }</Paper>
-          <Paper >Cant. : { option.StkEnvaseCant  }</Paper>
-          <Paper >Fecha : { option.StkEnvaseFechaAct  }</Paper>
-          <Paper >Ubicación : { option.StkEnvaseUbG }  -- {option.StkEnvaseUbF} </Paper>
-          <Paper >Observ. : { option.StkEnvaseObserv  }</Paper>
-        </Grid>
-          ))}
-      </Grid>
-               
-    </form>
-      {/* {this.stkenvasecambiaimp() } */}
-    {/* <button type="button" onClick={()=>printJS( 'codigo-QR', 'html')} >
-  
-    Print eeForm
- </button> */}
-  </div>       
-  // </div>   
+        <Button variant="contained" color="secondary" onClick={()=>this.props.cancelaImpresion()}>
+          Cancelar
+        </Button>
+    </div>   
     )
-       
-    }
-   
   }
-
-export default StkGenImpQR
+}
+export default withStyles(styles)(StkGenImpQR)	
