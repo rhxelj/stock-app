a887518307d20d24c49ef18d951619c77e9f451f
sandrapk <sperezk@yahoo.com.ar>
2020-01-14 08:49:33 -0300

    cambio de ruta de grupo
    en el front tuve que camiar StkGrupoModificar linea 207 ( document.getElementById("Grabar").focus(); ) tenía un espacio después de Grabar y no funcionaba con el Enter

---
+++ backend/routes/stock/grupos/stkleegrprubitem.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var router = express.Router();
+var path = require("path");
+var conexion = require("../../conexion");
+
+conexion.connect(function(err) {
+  if (!err) {
+    console.log("base de datos conectada en stkgrupoleercod");
+  } else {
+    console.log("no se conecto en stkgrupoleercod");
+  }
+});
+var GrupoDescripcion = "";
+var q;
+function leegrupo(codgrupo) {
+  //'Select StkGrupoDesc as GrupoDesc from StkGrupo  where idStkGrupo = ' + codgrupo,
+  q = [
+    "Select StkGrupoDesc as GrupoDesc",
+    "from StkGrupo  where idStkGrupo = ",
+    codgrupo
+  ].join(" ");
+  conexion.query(q, function(err, result) {
+    if (err) {
+      console.log("Error LEER StkGrupo");
+      console.log(err);
+    } else {
+      GrupoDescripcion = result[0].GrupoDesc;
+      console.log(GrupoDescripcion);
+    }
+  });
+  return GrupoDescripcion;
+}
+
+exports.GrupoDescripcion;
+//exports.leegrupo=leegrupo;
