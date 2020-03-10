760475a8aaa38e4dff0bfdbd43508137af353308
Rogelio Martin Corsino <rcorsino@gmail.com>
2020-03-10 16:34:14 -0300

    Cambios de layout
    Cambios esteticos en la posicion de algunos elementos

--- src/components/Main/pages/Movimientos/Entrada/LayOutOne/FilaDos/index.tsx
+++ src/components/Main/pages/Movimientos/Entrada/LayOutOne/FilaDos/index.tsx
@@ -13,7 +13,7 @@ export default function Fila2() {
 
   return (
     <>
-      <Grid container spacing={3}>
+      <Grid container justify="space-around" spacing={3}>
         <F2C1 />
         <F2C2 />
       </Grid>
