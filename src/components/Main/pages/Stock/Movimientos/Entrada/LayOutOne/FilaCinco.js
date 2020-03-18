79927aec4fe6d98b6027ec20adc09255a2be8c3c
Rogelio Martin Corsino <rcorsino@gmail.com>
2020-03-18 15:15:41 -0300

    Agregar Fila5 y agregar en docz combo botones

--- src/components/Main/pages/Stock/Movimientos/Entrada/LayOutOne/FilaCinco.js
+++ src/components/Main/pages/Stock/Movimientos/Entrada/LayOutOne/FilaCinco.js
@@ -1,29 +1,22 @@
 import React from "react";
 // import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
+import { Button, Paper } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import useStyles from "./styles";
-
+import ComboBCC from "../../../../../../lib/ComboBCC";
 export default function Fila() {
   const classes = useStyles();
+
+  const actions = {
+    borrar: () => alert("presiono BORRAR"),
+    confirmar: () => alert("presiono CONFIRMAR"),
+    cancelar: () => alert("presiono CANCELAR")
+  };
 
   return (
     <>
       <Grid container item spacing={2} justify="flex-end">
-        {/* <Grid container direction="row" justify="flex-end" spacing={4} item>
-          <Paper className={classes.paper}>Borrar</Paper>
-          <Paper className={classes.paper}>confirma</Paper>
-          <Paper className={classes.paper}>cancela</Paper>
-        </Grid> */}
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>Borrar</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>Confirma</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>Cancela</Paper>
-        </Grid>
+        <ComboBCC actions={actions} />
       </Grid>
     </>
   );
