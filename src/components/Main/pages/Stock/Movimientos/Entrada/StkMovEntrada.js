1fdb282cde663520b2022a862c7702bd01deb785
Rogelio Martin Corsino <rcorsino@gmail.com>
2020-03-20 14:12:42 -0300

    StkMovEntrada: Limpieza de componente

--- src/components/Main/pages/Stock/Movimientos/Entrada/StkMovEntrada.js
+++ src/components/Main/pages/Stock/Movimientos/Entrada/StkMovEntrada.js
@@ -23,48 +23,11 @@ import FilaCuatro from "./LayoutMovEntrada/FilaCuatro";
 import FilaCinco from "./LayoutMovEntrada/FilaCinco";
 import { initial_state } from "./Initial_State";
 
-// import { stkgrupolee } from "../../Grupos/StkGrupoLee";
-
 export const StkMovEntradaContext = React.createContext();
-
-// const GRI_initial_state = {
-//   idStkGrupo: "",
-//   idStkRubro: "",
-//   idStkItems: ""
-// };
-
-// const data_initial_state = {
-//   cantidad: "",
-//   StkRubroPres: "",
-//   StkItemsCantDisp: "",
-//   StkItemsCantidad: "",
-//   StkEnvasePartida: "",
-//   StkEnvaseUbG: "",
-//   StkEnvaseUbF: "",
-//   StkEnvaseObserv: ""
-// };
 
 var StkMovEntrada = props => {
   const [state, setState] = useState(initial_state);
-  // const [GRI, setGRI] = useState(GRI_initial_state); //la uso como variable para pasarla a la columna2
-
-  // var [GRI, setGRI] = useState(GRI_initial_state); //la uso como variable para pasarla a la columna2
-  // const [data, setData] = useState(data_initial_state);
-
   const classes = useStyles();
-
-  // async function stkgrupoleer() {
-  //   const result = await stkgrupolee();
-  //   setState({ ...state, stkgrupo: result });
-  // }
-  // useEffect(() => {
-  //   console.log("en stkmoventrada en state.stkgrupo  ");
-  //   console.log(state.stkgrupo);
-
-  //   if (state.stkgrupo.length === 0) {
-  //     stkgrupoleer();
-  //   }
-  // }, [state.stkgrupo]);
 
   return (
     <div>
@@ -74,13 +37,6 @@ var StkMovEntrada = props => {
             value={{
               state: state,
               setState: setState
-
-              // GRI: GRI,
-              // setGRI: setGRI
-              // data: GRI,
-              // stkgrupoleer: stkgrupoleer
-              // data: data,
-              // setData: setData,
             }}
           >
             <FilaUno /> {/* Fecha */}
