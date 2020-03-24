d32250b5ae04d519a1c38a95db5f57b26d124ed2
Rogelio Martin Corsino <rcorsino@gmail.com>
2020-03-24 14:26:41 -0300

    Cambio en formato de fecha en header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -20,16 +20,15 @@ import { format } from "fecha";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 }));
-
 
 const initial_state = {
   open: false,
@@ -76,9 +75,8 @@ function Header() {
   const handleClose = prop => event => {
     setState({ ...state, [prop]: null });
   };
-  const diafecha = format(new Date(), "dd-MM-YYYY");
+  const diafecha = format(new Date(), "DD-MM-YYYY");
   function newMethod(abrir_movimientos, abrir_tablas) {
-   
     return (
       <SwipeableDrawer
         open={state.left}
@@ -154,8 +152,8 @@ function Header() {
   return (
     <div>
       <AppBar position="static">
-  <Toolbar>
-  <IconButton
+        <Toolbar>
+          <IconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
@@ -167,12 +165,10 @@ function Header() {
             OlsaSG
           </Typography>
           {newMethod(abrir_movimientos, abrir_tablas)}
-          <Typography variant="h6" >
-          {diafecha}
-          </Typography>
+          <Typography variant="h6">{diafecha}</Typography>
           {/* <Button color="inherit">{diafecha}</Button> */}
-  </Toolbar>
-</AppBar>
+        </Toolbar>
+      </AppBar>
 
       {/* <AppBar position="static">
         <Toolbar>
