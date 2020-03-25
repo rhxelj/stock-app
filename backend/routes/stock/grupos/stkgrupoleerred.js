4711a21504c75d52c2f64e0228544227296d182d
Sandra <sperezk@yahoo.com.ar>
2020-03-25 16:48:04 -0300

    Lectura reducida en backend

---
+++ backend/routes/stock/grupos/stkgrupoleerred.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var router = express.Router();
+var path = require("path");
+var conexion = require("../../conexion");
+
+conexion.connect(function(err) {
+  if (!err) {
+    console.log("base de datos conectada en stkgrupoleer");
+  } else {
+    console.log("no se conecto en stkgrupoleer");
+  }
+});
+
+var router = express();
+
+router.get("/", function(req, res, next) {
+  //'Select * from StkGrupo '
+  var q = [
+    "Select idStkGrupo, StkGrupoDesc from StkGrupo order by idStkGrupo"
+  ].join(" ");
+  conexion.query(q, function(err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+conexion.end;
+module.exports = router;
