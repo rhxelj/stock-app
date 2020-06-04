1cf743ddd5aeb4ae6544659716c454a685f1d5ef
Rogelio M Corsino <rcorsino@gmail.com>
2020-06-04 16:28:52 -0300

    refactor(items):scar columns afuera de items

--- src/components/Main/pages/Stock/Items/StkItems.jsx
+++ src/components/Main/pages/Stock/Items/StkItems.jsx
@@ -22,7 +22,7 @@ import IpServidor from "../../VariablesDeEntorno";
 import MaterialTable, { Column } from "material-table";
 import { tableIcons } from "../../../../lib/material-table/tableIcons";
 import { localization } from "../../../../lib/material-table/localization";
-
+import { columns } from "./itemsColumns";
 // import 'react-table/react-table.css'
 // import ReactTable from 'react-table'
 // import Button from '@material-ui/core/Button';
@@ -65,8 +65,6 @@ const CustomTableCell = withStyles((theme) => ({
 
 // class StkItems extends Component {
 const initialState = {
-  // toggle: false,
-  togglemodificar: false,
   idStkItems: 0,
   StkItemsGrupo: 0,
   StkItemsRubro: 0,
@@ -78,17 +76,6 @@ const initialState = {
   items: [],
   itemsdetalles: [],
   stkgrupoitem: [],
-  toggle: {
-    agregar: false,
-    busqueda: false,
-    modificar: false,
-    seleccampos: false,
-  },
-  toggle_agregar: false,
-  toggle_busqueda: false,
-  toggle_modificar: false,
-  filtered: "",
-  direction: "asc",
 };
 
 function StkItems() {
@@ -219,111 +206,6 @@ function StkItems() {
   useEffect(() => {
     leeStkItemsDetalles();
   });
-
-  // render() {
-  //   // Agrego el campo del Boton BORRAR
-  //   this.state.items.map((rowData, index) =>
-  //     Object.assign(rowData, {
-  //       borrar: (
-  //         // <div className="center-align"><StkItemsBorrar StkItem={[rowData.idStkItems,rowData.StkItemsGrupo,rowData.StkItemsRubro]} leeStkItems={()=>this.leeStkItems()} read={()=>this.leeStkItems()}></StkItemsBorrar></div>})
-  //         <div className="center-align">
-  //           <StkItemsBorrar
-  //             idStkItems={rowData.idStkItems}
-  //             StkItemsGrupo={rowData.StkItemsGrupo}
-  //             StkItemsRubro={rowData.StkItemsRubro}
-  //             leeStkItems={() => this.leeStkItems()}
-  //             read={() => this.leeStkItems()}
-  //           ></StkItemsBorrar>
-  //         </div>
-  //       ),
-  //     })
-  //   );
-  // Tengo que hacer otro Object.assin para  StkItemsGrupo para mostrar la descripcion o lo hacemos desde el backend?
-
-  //  Esto se agrega dentro de la funcion render
-
-  // Filtrado de datos - Begin
-
-  // var items = this.state.items.filter((item) => {
-  //   return (
-  //     item.StkItemsDesc.toLowerCase().indexOf(
-  //       this.state.filtered.toLowerCase()
-  //     ) !== -1 ||
-  //     item.StkRubroDesc.toLowerCase().indexOf(
-  //       this.state.filtered.toLowerCase()
-  //     ) !== -1 ||
-  //     item.StkGrupoDesc.toLowerCase().indexOf(
-  //       this.state.filtered.toLowerCase()
-  //     ) !== -1 ||
-  //     // item.StkItemsGrupo.indexOf(this.state.filtered) !== -1
-  //     // item.StktemsFAct.toLowerCase().indexOf(this.state.filtered.toLowerCase()) !== -1
-  //     item.StkItemsFAct.indexOf(this.state.filtered) !== -1
-  //   );
-  // });
-  // Filtrado de datos - End
-
-  // Encabezado de la Tabla
-  var columns = [
-    {
-      title: "Items(ID)",
-      field: "idStkItems",
-      tipo: "numero",
-      order: true,
-    },
-    {
-      title: "Grupo",
-      field: "StkGrupoDesc",
-      tipo: "numero",
-      order: true,
-    },
-    {
-      title: "Rubro",
-      field: "StkRubroDesc",
-      tipo: "texto",
-      order: true,
-    },
-    {
-      title: "Descripción",
-      field: "StkItemsDesc",
-      tipo: "texto",
-      order: true,
-    },
-    {
-      title: "Cantidad",
-      field: "StkItemsCantidad",
-      tipo: "numero",
-      order: true,
-    },
-    {
-      title: "Cantidad Disponible",
-      field: "StkItemsCantDisp",
-      tipo: "numero",
-      order: true,
-    },
-    {
-      title: "Fecha de Actualización",
-      field: "StkItemsFAct",
-      tipo: "texto",
-      order: true,
-    },
-    {
-      title: "Stock Mínimo",
-      field: "StkItemsMin",
-      tipo: "numero",
-      order: true,
-    },
-    {
-      title: "Stock Máximo",
-      field: "StkItemsMax",
-      tipo: "numero",
-      order: true,
-    },
-    // {
-    //     title: "Observaciones",
-    //     field: "StkItemsObserv",
-    //     tipo:"numero"
-    // },
-  ];
 
   return (
     <div>
---
+++ src/components/Main/pages/Stock/Items/itemsColumns.js
@@ -0,0 +1,61 @@
+export const columns = [
+  {
+    title: "Items(ID)",
+    field: "idStkItems",
+    tipo: "numero",
+    order: true,
+  },
+  {
+    title: "Grupo",
+    field: "StkGrupoDesc",
+    tipo: "numero",
+    order: true,
+  },
+  {
+    title: "Rubro",
+    field: "StkRubroDesc",
+    tipo: "texto",
+    order: true,
+  },
+  {
+    title: "Descripción",
+    field: "StkItemsDesc",
+    tipo: "texto",
+    order: true,
+  },
+  {
+    title: "Cantidad",
+    field: "StkItemsCantidad",
+    tipo: "numero",
+    order: true,
+  },
+  {
+    title: "Cantidad Disponible",
+    field: "StkItemsCantDisp",
+    tipo: "numero",
+    order: true,
+  },
+  {
+    title: "Fecha de Actualización",
+    field: "StkItemsFAct",
+    tipo: "texto",
+    order: true,
+  },
+  {
+    title: "Stock Mínimo",
+    field: "StkItemsMin",
+    tipo: "numero",
+    order: true,
+  },
+  {
+    title: "Stock Máximo",
+    field: "StkItemsMax",
+    tipo: "numero",
+    order: true,
+  },
+  // {
+  //     title: "Observaciones",
+  //     field: "StkItemsObserv",
+  //     tipo:"numero"
+  // },
+];
