9f3e278a4ec7774119d72511bf1aa765818a2d43
Rogelio M Corsino <rcorsino@gmail.com>
2020-06-30 14:40:18 -0300

    refactor(StkRubro):Arreglar el refresco de los ABM

--- src/components/Main/pages/Stock/Rubros/StkRubro.js
+++ src/components/Main/pages/Stock/Rubros/StkRubro.js
@@ -1,12 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-// import { stkGrupoLeerRedRubro } from "../Grupos/StkGrupoLeerRedRubro";
-// import { stkrubroleeproveedor } from "./StkRubroLeeProveedor";
-
-// import { stkUnMedLeerRed } from "./StkUnMedLeerRed";
-// import { stkMonedasleerRed } from "./StkMonedasLeerRed";
-import { buscaCodigo } from "./BuscaCodigo";
-
 import { StkRubro_Columns } from "./StkRubro_Columns";
 
 import { tableIcons } from "../../../../lib/material-table/tableIcons";
@@ -15,12 +8,7 @@ import { stkrubroleermezcla } from "./StkRubroLeerMezcla";
 
 import { onRowAdd } from "./onRowAdd";
 import { onRowUpdate } from "./onRowUpdate";
-// import { onRowDelete } from "./onRowDelete"
-
-// import { agregarRubros } from "./StkRubroAgregar";
-import { borrarRubros } from "./StkRubroBorrar";
-// import { modificarRubros } from "./StkRubroModificar";
-
+import { onRowDelete } from "./onRowDelete";
 import MaterialTable, { Column } from "material-table";
 import { HeaderTitle } from "../../../../lib/HeaderTitle";
 
@@ -55,40 +43,9 @@ export default function StkRubro() {
     dataFetch(); //Lleno data
   }
 
-  // revisado -------
-
   useEffect(() => {
     initialFetch();
   }, []);
-
-  // function onRowadd(newData) {
-  //   return new Promise((resolve) => {
-  //     setTimeout(() => {
-  //       {
-  //         agregarRubros(newData).then(() => dataFetch());
-  //       }
-  //       resolve();
-  //     }, 600);
-  //   });
-  // }
-
-  // function onRowUpdate(newData, oldData) {
-  //   return new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       modificarRubros(newData).then(() => dataFetch());
-  //       resolve();
-  //     }, 600);
-  //   });
-  // }
-
-  function onRowDelete(oldData) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        borrarRubros(oldData).then(() => dataFetch());
-      }, 600);
-      resolve();
-    });
-  }
 
   return (
     <div>
@@ -109,11 +66,26 @@ export default function StkRubro() {
         //   // <StkRubroModificar open={true} />,
         // }}
         editable={{
-          onRowAdd: (newData) => onRowAdd(newData).then(() => dataFetch()),
-          onRowUpdate: (newData, oldData) =>
-            onRowUpdate(newData, oldData).then(() => dataFetch()),
+          onRowAdd: (newData) =>
+            onRowAdd(newData).then(
+              setTimeout(() => {
+                {
+                  initialFetch();
+                }
+              }, 5000)
+            ),
 
-          onRowDelete: (oldData) => onRowDelete(oldData),
+          onRowUpdate: (newData, oldData) =>
+            onRowUpdate(newData, oldData).then(() => initialFetch()),
+
+          onRowDelete: (oldData) =>
+            onRowDelete(oldData).then(
+              setTimeout(() => {
+                {
+                  initialFetch();
+                }
+              }, 5000)
+            ),
         }}
       />
     </div>
---
+++ src/components/Main/pages/Stock/Rubros/onRowDelete.js
@@ -0,0 +1,12 @@
+import { borrarRubros } from "./StkRubroBorrar";
+
+export function onRowDelete(oldData) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("oldata ");
+      console.log(oldData);
+      borrarRubros(oldData);
+    }, 600);
+    resolve();
+  });
+}
